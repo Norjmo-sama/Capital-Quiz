617023312b0382d17d78a273e1d46aa866b9d33c
Molly <norman.j.molly@gmail.com>
2024-10-14 07:54:51 -0600

    peen

---
+++ Capital Quiz/main.swift
@@ -0,0 +1,99 @@
+//
+//  main.swift
+//  Tic-Tac-Toe
+//
+//  Created by Molly Norman on 10/11/24.
+//
+
+import Foundation
+
+print("Welcome to Captail Quiz!")
+print("What is the capital of France?")
+print("1. Berlin")
+print("2. Madrid")
+print("3. Paris")
+print("4. Rome")
+let answer1 = readLine()
+if let answer1 = answer1 {
+    if (answer1.lowercased().contains("Paris")) {
+        exit(0)
+    }
+}
+print("What is the capital of South Korea?")
+print("1. Seoul")
+print("2. Busan")
+print("3. Incheon")
+print("4. Gwangju")
+let answer2 = readLine()
+if let answer2 = answer2 {
+    if (answer2.lowercased().contains("Seoul")) {
+        exit(0)
+    }
+}
+print("What is the capital of Puerto Rico?")
+print("1. Fajardo")
+print("2. San Juan")
+print("3. Ponce")
+print("4. Bayamón")
+let answer3 = readLine()
+if let answer3 = answer3 {
+      if (answer3.lowercased().contains("San Juan")) {
+        exit(0)
+    }
+}
+print("What is the capital of Canada?")
+print("1. Vancouver")
+print("2. Montreal")
+print("3. Toronto")
+print("4. Ottawa")
+let answer4 = readLine()
+if let answer4 = answer4 {
+      if (answer4.lowercased().contains("Ottawa")) {
+        exit(0)
+    }
+}
+print("Let's make this harder 😈")
+print("What is the capital of Tuvalu?")
+print("1. Senala")
+print("2. Funafuti")
+print("3. Vaiaku")
+print("4. Tonga")
+let answer5 = readLine()
+if let answer5 = answer5 {
+      if (answer5.lowercased().contains("Funafuti")) {
+        exit(0)
+    }
+}
+print("What is the capital of Marshall Islands?")
+print("1. Majuro")
+print("2. Ajeltake")
+print("3. Jaluit")
+print("4. Ebeye")
+let answer6 = readLine()
+if let answer6 = answer6 {
+      if (answer6.lowercased().contains("Majuro")) {
+        exit(0)
+    }
+}
+print("What is the capital of Kyrgyzstan?")
+print("1. Karakol")
+print("2. Naryn")
+print("3. Bishkek")
+print("4. Uzgen")
+let answer7 = readLine()
+if let answer7 = answer7 {
+      if (answer7.lowercased().contains("Bishkek")) {
+        exit(0)
+    }
+}
+print("What is the capital of Brunei?")
+print("1. Bandar Seri Begawan")
+print("2. Bangar")
+print("3. Kuala Belait")
+print("4. Seria")
+let answer8 = readLine()
+if let answer8 = answer8 {
+      if (answer8.lowercased().contains("Bandar Seri Begawan")) {
+        exit(0)
+    }
+}
